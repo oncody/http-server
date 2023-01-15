4bf855ba4ef8875d26ccdf6b12d3f7c08cb2ad49
Cody Smith <37386486+oncody@users.noreply.github.com>
2023-01-15 10:23:12 -0700

    initial files

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+export default class Server {
+    /**
+     * @param {number} port
+     */
+    constructor(port) {
+        this._server = express();
+        this._port = port;
+    }
+
+    /**
+     * @returns {undefined}
+     */
+    enableCrossOriginResourceSharing() {
+        this._server.use(cors());
+    }
+
+    /**
+     * Register a get route
+     * @param {string} route
+     * @param {Function} func. This takes in request and response
+     */
+    registerGetRoute(route, func) {
+        this._server.get(route, func);
+    }
+
+    /**
+     * Start listening and serving traffic
+     */
+    start() {
+        this._server.listen(this._port);
+    }
+}
